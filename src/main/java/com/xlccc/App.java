c57707f138bd76d13bc0c684902460fed389d158
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-02-03 22:23:21 +0800

    Initial commit

---
+++ src/main/java/com/xlccc/App.java
@@ -0,0 +1,20 @@
+package com.xlccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class App {
+
+    @RequestMapping("/")
+    public String home (){
+        return "Hello world";
+    }
+    public static void main ( String [] args){
+        System.out.println("Hello world ");
+        SpringApplication.run(App.class , args);
+    }
+}
